9e14e998d36b42f7ad496579faa3ae302bd330df
ausikek <orapacen2@outlook.com>
2025-01-01 17:53:20 -0300

    fix: add button not showing up when there are no tasks

--- src/components/NoTasks/index.tsx
+++ src/components/NoTasks/index.tsx
@@ -1,17 +1,19 @@
-import { Input } from "@/components/ui/input";
-import StatusFilter from "@/components/StatusFilter";
-import { Plus } from "lucide-react";
+import { Input } from '@/components/ui/input';
+import StatusFilter from '@/components/StatusFilter';
+import { Plus } from 'lucide-react';
+import AddTaskButton from '../AddTask';
 
 export default function NoTasks() {
   return (
-    <div className="w-full flex flex-col gap-28">
-      <div className="flex flex-row gap-4">
-        <Input placeholder="Pesquisar..." className="w-40 lg:w-1/4" />
-        <StatusFilter status={"x"} setStatus={() => {}} />
+    <div className='w-full flex flex-col gap-28'>
+      <div className='flex flex-row gap-4'>
+        <Input placeholder='Pesquisar...' className='w-40 lg:w-1/4' />
+        <StatusFilter status={'x'} setStatus={() => {}} />
+        <AddTaskButton />
       </div>
-      <div className="flex flex-col gap-4 w-full items-center">
+      <div className='flex flex-col gap-4 w-full items-center'>
         <h1>Você não possui tasks cadastradas</h1>
-        <h2 className="flex gap-2 flex-row">
+        <h2 className='flex gap-2 flex-row'>
           Adicione tasks clicando no botão <Plus />
         </h2>
       </div>
